ade38efe89ac31a9b3bdb681a2a5d62b27e45a3c
Ari Asulin <ari.asulin@gmail.com>
2024-01-03 19:50:23 -0500

    testing vercel

--- next.config.js
+++ next.config.js
@@ -16,6 +16,7 @@ const withMDX = require('@next/mdx')({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
     formats: ['image/avif', 'image/webp'],
     domains: ['i.ibb.co']
@@ -30,10 +31,14 @@ const nextConfig = {
   },
   // output: {path: path.resolve(__dirname, 'static'),},
   webpack: (config, options) => {
-    // config.output.filename = '[path][name].[hash].[ext]';
-    // config.output.path = path.resolve(__dirname, '.next/')
-    // config.output.publicPath = '/.next/'
+    //   // config.output.filename = '[path][name].[hash].[ext]';
+    //   // config.output.path = path.resolve(__dirname, '.next/')
+    //   // config.output.publicPath = '/.next/'
     const nextImageLoader = config.module.rules.find(rule => rule.loader === 'next-image-loader')
+    // config.module.rules.push({
+    //   ...nextImageLoader,
+    //   test: /\.(webm|pdf|txt|csv)/,
+    // })
     nextImageLoader.loader = path.join(process.cwd(), 'server/imageLoader.js')
     config.module.rules.push({
       test: /\.(webm|pdf|txt|csv)/,
@@ -45,26 +50,26 @@ const nextConfig = {
       //     filename: '.next/static/[path][name].[hash].[ext]'
       // },
     })
-
+    //
     return config
-  },
-  async headers () {
-    return [
-      {
-        // matching all API routes
-        source: '/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' }, // replace this your actual origin
-          { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-          }
-        ]
-      }
-    ]
   }
+  // async headers () {
+  //   return [
+  //     {
+  //       // matching all API routes
+  //       source: '/api/:path*',
+  //       headers: [
+  //         { key: 'Access-Control-Allow-Credentials', value: 'true' },
+  //         { key: 'Access-Control-Allow-Origin', value: '*' }, // replace this your actual origin
+  //         { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
+  //         {
+  //           key: 'Access-Control-Allow-Headers',
+  //           value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  //         }
+  //       ]
+  //     }
+  //   ]
+  // }
 }
 
 // Merge MDX config with Next.js config
