717d1ef0f91049010beb84e6f7854ee70c0a415a
Ari Asulin <ari.asulin@gmail.com>
2024-03-10 12:19:08 -0400

    Added description of period before the Golden Age

--- components/Image/OptimizedImage.js
+++ components/Image/OptimizedImage.js
@@ -5,14 +5,15 @@ import { processImageProps } from '@/components/Image/imgUtil'
 export default function OptimizedImage ({ children, className, ...props }) {
   const srcProps = processImageProps(props)
 
-  const { alt, priority, blurDataURL, optimizedSrc, ...finalProps } = srcProps
+  const { alt, priority, blurDataURL, optimizedSrc, format, ...finalProps } = srcProps
 
   if (blurDataURL) { finalProps.style = { backgroundImage: `url('${blurDataURL}')` } }
-  if (typeof finalProps.loading === 'undefined') { finalProps.loading = 'lazy' }
+  // if (typeof finalProps.loading === 'undefined') { finalProps.loading = 'lazy' }
   if (optimizedSrc) { finalProps.src = optimizedSrc }
 
   let content = (
     <img
+      loading="lazy"
       alt={alt}
       className={`${styles.image} ${className || ''}`}
       {...finalProps}
@@ -26,6 +27,7 @@ export default function OptimizedImage ({ children, className, ...props }) {
         style={{ maxWidth: finalProps.width + 'px' }}
       >
         <img
+          loading="lazy"
           alt={alt}
           className={styles.image}
           {...finalProps}
